a837db152c5abc6035fb0289828e785a32782fc8
Eric Favre <efavre@octo.com>
2016-04-22 17:31:08 +0200

    add react-rails

--- app/assets/javascripts/application.js.jsx
+++ app/assets/javascripts/application.js.jsx
@@ -13,4 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
 //= require_tree .
+
+var APPLICATIONS = {"ccu":[{"environment":"prod","version":"1.T.4"}],"myApplication2":[{"environment":"Preproduction","version":"2.3.4"},{"environment":"Production","version":"2.3.5"}],"MyApplication":[{"environment":"Production","version":"1.2.4"},{"environment":"Preproduction","version":"1.2.3"}]};
+
+ReactDOM.render(
+<Dashboard applicationsMap={APPLICATIONS} />,
+    document.getElementById('dashboard')
+);
