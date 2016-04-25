203160bfad5cf66fd6ed81850026f67e0f7d1ecc
Eric Favre <efavre@octo.com>
2016-04-25 11:50:16 +0200

    precompile assetss

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # dashboard.js.jsx, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( application.js )
